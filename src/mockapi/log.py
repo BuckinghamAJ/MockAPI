37261742d2cdc25fbbacf97edb2a93bc432dc15f
Adam Buckingham <48027257+BuckinghamAJ@users.noreply.github.com>
2023-02-05 20:29:25 -0500

    Added logging

---
+++ src/mockapi/log.py
@@ -0,0 +1,86 @@
+"""
+Pulled code from FASTApi docs:
+https://pawamoy.github.io/posts/unify-logging-for-a-gunicorn-uvicorn-app/
+"""
+
+import os
+import logging
+import sys
+
+from gunicorn.glogging import Logger
+from loguru import logger
+
+LOG_LEVEL = logging.getLevelName(os.environ.get("LOG_LEVEL", "DEBUG"))
+JSON_LOGS = True if os.environ.get("JSON_LOGS", "0") == "1" else False
+WORKERS = int(os.environ.get("GUNICORN_WORKERS", "5"))
+
+"""
+logging process:
+import logging
+
+log = logging.getLogger(__name__)
+"""
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+class StubbedGunicornLogger(Logger):
+    def setup(self, cfg):
+        handler = logging.NullHandler()
+        self.error_logger = logging.getLogger("gunicorn.error")
+        self.error_logger.addHandler(handler)
+        self.access_logger = logging.getLogger("gunicorn.access")
+        self.access_logger.addHandler(handler)
+        self.error_logger.setLevel(LOG_LEVEL)
+        self.access_logger.setLevel(LOG_LEVEL)
+
+
+def configure_logger() -> dict:
+    intercept_handler = InterceptHandler()
+    # logging.basicConfig(handlers=[intercept_handler], level=LOG_LEVEL)
+    # logging.root.handlers = [intercept_handler]
+    logging.root.setLevel(LOG_LEVEL)
+
+    seen = set()
+    for name in [
+        *logging.root.manager.loggerDict.keys(),
+        "gunicorn",
+        "gunicorn.access",
+        "gunicorn.error",
+        "uvicorn",
+        "uvicorn.access",
+        "uvicorn.error",
+    ]:
+        if name not in seen:
+            seen.add(name.split(".")[0])
+            logging.getLogger(name).handlers = [intercept_handler]
+
+    logger.configure(handlers=[{"sink": sys.stdout, "serialize": JSON_LOGS}])
+
+    options = {
+        "bind": "0.0.0.0",
+        "workers": WORKERS,
+        "accesslog": "-",
+        "errorlog": "-",
+        "worker_class": "uvicorn.workers.UvicornWorker",
+        "logger_class": StubbedGunicornLogger,
+    }
+
+    return options
